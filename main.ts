52cd1ab6edc5dd7a0420cb35a610126fb6cba003
Areeba Ahmed khan <159721838+gulariba@users.noreply.github.com>
2024-03-16 22:07:40 +0500

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,25 @@
+let userName :string ="Areeba Ahmed khan";
+console.log(userName)
+
+let age: number =21
+console.log(age) 
+
+let a : string="pakistan"
+let b : number=10.9
+let c : boolean=false
+
+let m ="USA"
+let n =10
+let o=false
+
+console.log(a)
+console.log(b)
+console.log(c)
+
+let currentbalance =1000
+const accountnumber =123456789
+
+let firstname :string="Muhammad waseem"
+let lastname :string=" rasool"
+let fullname :string=firstname+lastname
+console.log(fullname)
